d7b3efbcbba5d31e6c7a6807633e9b3995bf4a1f
A.I <ailis@paw.zone>
2015-07-19 20:04:01 -0700

    Force to exec with sudo

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"os/user"
 
 	"github.com/ailispaw/xhyvectl/vmnet"
 )
@@ -15,6 +17,26 @@ func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s <UUID>\n", os.Args[0])
 		os.Exit(1)
+	}
+
+	user, err := user.Current()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: Could not get the current user\n", COMMAND)
+		os.Exit(1)
+	}
+	if user.Uid != "0" {
+		fmt.Fprintf(os.Stderr, "%s: Requires root permissions\n", COMMAND)
+		fmt.Fprintf(os.Stderr, "sudo")
+		for _, arg := range os.Args {
+			fmt.Fprintf(os.Stderr, " %s", arg)
+		}
+		fmt.Fprintf(os.Stderr, "\n")
+		out, err := exec.Command("sudo", os.Args...).Output()
+		if err != nil {
+			os.Exit(1)
+		}
+		fmt.Printf("%s", string(out))
+		os.Exit(0)
 	}
 
 	mac, err := vmnet.GetMACAddressFromUUID(os.Args[1])
